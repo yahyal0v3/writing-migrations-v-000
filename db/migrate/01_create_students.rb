0a23fbf3004950d285f035e05dc651b340463a9c
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-12-07 19:31:10 +0000

    automatically backed up by learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,4 +1,11 @@
 class CreateStudents < ActiveRecord::Migration
 
+  def change
+    create_table :students do |t|
+      t.string :name
+      t.integer :age
+      t.string :hometown
+    end
+  end
 
 end
